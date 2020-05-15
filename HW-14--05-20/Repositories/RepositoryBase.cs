fd3a81f6874c0f2ea371183f1839a4101c972f08
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-15 19:40:20 +0500

    completed

---
+++ HW-14--05-20/Repositories/RepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace HW_14__05_20.Repositories
+{
+    public abstract class RepositoryBase<T>
+    {
+        public string ConnString { get; private set; }
+        public RepositoryBase()
+        {
+            ConnString = "Data source=localhost; Initial catalog=AlifAcademy; Integrated security=true";
+        }
+        public List<T> SelectAll()
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(ConnString))
+                {
+                    return db.Query<T>($"SELECT * FROM {typeof(T).Name?.ToUpper()}").ToList();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Error:{ex.Message}");
+                return null;
+            }
+        }
+    }
+}
